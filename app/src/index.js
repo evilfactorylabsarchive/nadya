eade13e06621f2d441cadc660e2bfe2a23a1d4d7
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-06-19 21:51:15 +0700

    feat: configure @reach/router to the app. Close #12

--- app/src/index.js
+++ app/src/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+
 import { render } from 'react-dom'
+import { Router } from '@reach/router'
+import { Add, Edit, Pick, Shell } from './fragments'
 
-import App from './App'
 import * as serviceWorker from './serviceWorker'
+import './App.css'
 
-render(<App />, document.getElementById('app'))
+render(
+  <>
+    <Navbar />
+    <Router>
+      <Shell path='/' />
+      <Pick path='/pick' />
+      <Add path='/pick/:service_id' />
+      <Edit path='/edit/:id' />
+    </Router>
+  </>,
+  document.getElementById('app')
+)
 
 serviceWorker.unregister()
