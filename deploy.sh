2bda66450ae20a2495def8ab10541e3d4baff6ed
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-06-21 17:11:40 +0700

    ci: configure travis ci

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+# check is $NOW_TOKEN exists?
+if [[ -z "$NOW_TOKEN" ]]; then
+  echo
+  echo "> NOW_TOKEN is empty. Deployment cancelled."
+  echo
+  exit 1
+fi
+
+# is this on master branch or something else?
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
+  ENV_TARGET=production
+else
+  ENV_TARGET=staging
+fi
+
+# deploy web to target
+echo "> Deploying app with $ENV_TARGET environment..."
+cd app && now --target $ENV_TARGET --token=$NOW_TOKEN
+
+# deploy app to target
+echo "> Deploying wen with $ENV_TARGET environment..."
+cd ../web && now --target $ENV_TARGET --token=$NOW_TOKEN
+
+echo "> Done"
