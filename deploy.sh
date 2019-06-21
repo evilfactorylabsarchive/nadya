09ea245dceda8f9e3583fc38a3c13e8ce7418ac2
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-06-21 19:28:20 +0700

    ci: simplify script

--- deploy.sh
+++ deploy.sh
@@ -13,20 +13,23 @@ else
   ENV_TARGET=staging
 fi
 
-CHANGES=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master))
+CHANGES=$(git --no-pager diff --name-only $TRAVIS_COMMIT_RANGE)
 
-if [[ -n "$(grep '^web') <<< "$CHANGES")" ]]; then
-  # deploy web to target
+deploy_web() {
+  # deploy web to target environment
   echo "> Deploying app with $ENV_TARGET environment..."
   cd ./web && now --target $ENV_TARGET --token=$NOW_TOKEN --scope evilfactory
   cd ..
-fi
+}
 
-if [[ -n "$(grep '^app') <<< "$CHANGES")" ]]; then
-  # deploy app to target
+deploy_app() {
+  # deploy app to target environment
   echo "> Deploying web with $ENV_TARGET environment..."
   cd app && now --target $ENV_TARGET --token=$NOW_TOKEN --scope evilfactory
   cd ..
-fi
+}
+
+[ -n "$(grep '^web' <<< "$CHANGES")" ] && deploy_web
+[ -n "$(grep '^app' <<< "$CHANGES")" ] && deploy_app
 
 echo "> Done"
