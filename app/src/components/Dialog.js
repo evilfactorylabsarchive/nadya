84e3c414472e9140f3cbb0d5b68aff5d7e4e53fd
ri7nz <ri7nz.labs@gmail.com>
2019-06-25 02:10:10 +0700

    added: dialog component & change shell

---
+++ app/src/components/Dialog.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Button from '@material-ui/core/Button'
+import Dialog from '@material-ui/core/Dialog'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import IconButton from '@material-ui/core/IconButton'
+import Typography from '@material-ui/core/Typography'
+import CloseIcon from '@material-ui/icons/Close'
+import Slide from '@material-ui/core/Slide'
+
+const useStyles = makeStyles(theme => ({
+  appBar: {
+    position: 'relative'
+  },
+  title: {
+    marginLeft: theme.spacing(2),
+    flex: 1
+  }
+}))
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction='up' ref={ref} {...props} />
+})
+
+export default function FullScreenDialog({ open, handleClose, title }) {
+  const classes = useStyles()
+  return (
+    <Dialog
+      fullScreen
+      open={open}
+      onClose={handleClose}
+      TransitionComponent={Transition}
+    >
+      <AppBar className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            edge='start'
+            color='inherit'
+            onClick={handleClose}
+            aria-label='Close'
+          >
+            <CloseIcon />
+          </IconButton>
+          <Typography variant='h6' className={classes.title}>
+            {title}
+          </Typography>
+          <Button color='inherit' onClick={handleClose}>
+            save
+          </Button>
+        </Toolbar>
+      </AppBar>
+    </Dialog>
+  )
+}
+
+FullScreenDialog.defaultProps = {
+  title: 'Screen Title'
+}
