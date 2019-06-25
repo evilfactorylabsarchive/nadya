173ab729f2bd37464b0169f00d2f191ac554ea5e
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-06-25 18:47:03 +0700

    feat: add Onboarding fragment to the route

--- app/src/fragments/index.js
+++ app/src/fragments/index.js
@@ -2,5 +2,6 @@ import Add from './Add'
 import Edit from './Edit'
 import Pick from './Pick'
 import Shell from './Shell'
+import Onboarding from './Onboarding'
 
-export { Add, Edit, Pick, Shell }
+export { Add, Edit, Pick, Shell, Onboarding }
