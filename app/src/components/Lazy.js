0502e0d731677535856834d424edfd08290e5368
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-06-26 07:16:03 +0700

    feat(app): create Lazy component for Suspense wrapper

---
+++ app/src/components/Lazy.js
@@ -0,0 +1,24 @@
+import React, { Suspense } from 'react'
+import LinearProgress from '@material-ui/core/LinearProgress'
+
+function Loading() {
+  return (
+    <LinearProgress
+      style={{
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%'
+      }}
+    />
+  )
+}
+
+export default function Lazy({ component, ...props }) {
+  const Lazy = React.lazy(() => import(`${component}`))
+  return (
+    <Suspense fallback={<Loading />}>
+      <Lazy {...props} />
+    </Suspense>
+  )
+}
