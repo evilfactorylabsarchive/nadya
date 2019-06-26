427628c0e9d26d8048f0f124e41b424b82e660ea
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-06-26 19:29:33 +0700

    feat: display list subscriptions. fix #7

--- app/src/fragments/Shell.js
+++ app/src/fragments/Shell.js
@@ -1,52 +1,122 @@
 import React, { Component } from 'react'
 import Fab from '@material-ui/core/Fab'
 import AddIcon from '@material-ui/icons/Add'
-
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardHeader from '@material-ui/core/CardHeader'
+import Avatar from '@material-ui/core/Avatar'
+import SimpleIcons from 'simple-icons-react-component'
 import { navigate } from '@reach/router'
+
 import { checkLogin } from '../services/user'
 
 import DialogLazy from '../components/Lazy'
+import {
+  listSubscription,
+  listenUpdate,
+  unlistenUpdate
+} from 'services/subscription'
+import { toCurrency } from 'utils'
 
 const classes = {
+  card: {
+    minWidth: 275
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)'
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  },
   fab: {
     position: 'fixed',
-    bottom: 10,
-    right: 10
+    bottom: 20,
+    right: 20
   },
   content: {
     padding: '2rem'
+  },
+  container: {
+    maxHeight: '90vh',
+    overflowY: 'auto'
   }
 }
 
 export default class App extends Component {
   state = {
-    isDialogOpen: false
+    isDialogOpen: false,
+    subscriptions: []
   }
 
   componentDidMount() {
     // FIXME(@108kb): delete this validation
     // after @ri7nz create HoC
-    checkLogin()
-      .then(user => {
-        console.log(user)
-      })
-      .catch(_ => {
-        // TODO(@108kb): Redirect to 4xx/5xx route
+
+    checkLogin().then(user => {
+      if (!user.total_rows) {
         navigate('/onboarding')
+      }
+    })
+
+    listSubscription().then(doc => {
+      this.setState({ subscriptions: doc.rows })
+    })
+
+    listenUpdate(doc => {
+      this.setState({
+        subscriptions: [doc, ...this.state.subscriptions]
       })
+    })
   }
+
   _handleDialogOpen = () => {
     this.setState({ isDialogOpen: true })
   }
+
   _handleDialogClose = () => {
     this.setState({ isDialogOpen: false })
   }
+
+  componentWillUnmount() {
+    unlistenUpdate()
+  }
+
   render() {
     return (
       <div className='App'>
-        <header style={classes.content}>
-          <p>I'm empty state</p>
-        </header>
+        {!this.state.subscriptions.length && (
+          <header style={classes.content}>
+            <p>I'm empty state</p>
+          </header>
+        )}
+        <div style={classes.container}>
+          {this.state.subscriptions
+            .filter(({ doc }) => Object.keys(doc).length !== 0)
+            .map(({ doc }) => (
+              <Card className={classes.card} key={doc._id}>
+                <CardHeader
+                  avatar={
+                    <CardActionArea>
+                      <Avatar
+                        style={{ backgroundColor: 'transparent' }}
+                        aria-label='Recipe'
+                        className={classes.avatar}
+                      >
+                        <SimpleIcons name={doc.title} />
+                      </Avatar>
+                    </CardActionArea>
+                  }
+                  title={doc.title}
+                  subheader={toCurrency(doc.cost)}
+                />
+              </Card>
+            ))}
+        </div>
         <Fab
           onClick={this._handleDialogOpen}
           color='primary'
