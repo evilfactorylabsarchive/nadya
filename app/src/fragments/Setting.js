8a47eef88914076f08a069b72ad26b50a78bc397
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-06-27 23:36:26 +0700

    feat: mount /setting route to app

---
+++ app/src/fragments/Setting.js
@@ -0,0 +1,109 @@
+import React, { useEffect, useState } from 'react'
+import dayjs from 'dayjs'
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  TextField,
+  Avatar,
+  Grid
+} from '@material-ui/core'
+
+import { deepOrange } from '@material-ui/core/colors'
+import { makeStyles } from '@material-ui/core/styles'
+
+import { getUser } from 'services/user'
+
+const useStyles = makeStyles(theme => ({
+  avatar: {
+    width: 90,
+    height: 90,
+    fontSize: 48,
+    marginTop: theme.spacing(4),
+    backgroundColor: deepOrange[500],
+    color: '#fff'
+  },
+  input: {
+    marginTop: theme.spacing(2)
+  },
+  action: {
+    marginLeft: 'auto',
+    marginRight: theme.spacing(1),
+    marginBottom: theme.spacing(1)
+  }
+}))
+
+export default () => {
+  const classes = useStyles()
+  const [user, setUser] = useState({})
+  const [userName, setUserName] = useState('')
+
+  useEffect(() => {
+    getUser().then(user => {
+      setUser(user)
+      setUserName(user.name)
+    })
+  }, [])
+
+  const handleSave = () => {
+    const payload = {
+      _id: user._id,
+      _rev: user._rev,
+      created_at: user.created_at,
+      name: userName
+    }
+    // TODO(108kb): handle update here
+  }
+
+  return (
+    <>
+      <Card>
+        <Grid container justify='center' alignItems='center'>
+          <Avatar className={classes.avatar}>
+            {userName ? userName.charAt(0).toUpperCase() : 'N'}
+          </Avatar>
+        </Grid>
+        <CardContent>
+          <TextField
+            className={classes.input}
+            variant='outlined'
+            value={user._id}
+            label='User ID'
+            margin='dense'
+            fullWidth={true}
+            disabled={true}
+          />
+          <TextField
+            className={classes.input}
+            variant='outlined'
+            value={dayjs(user.created_at).format('DD MMMM YYYY')}
+            label='Member sejak'
+            margin='dense'
+            fullWidth={true}
+            disabled={true}
+          />
+          <TextField
+            className={classes.input}
+            variant='outlined'
+            value={userName}
+            label='Nama'
+            margin='dense'
+            onChange={e => setUserName(e.target.value)}
+            fullWidth={true}
+          />
+        </CardContent>
+        <CardActions>
+          <Button
+            className={classes.action}
+            color='primary'
+            variant='contained'
+            onClick={handleSave}
+          >
+            Simpan
+          </Button>
+        </CardActions>
+      </Card>
+    </>
+  )
+}
---
+++ app/src/fragments/Setting.test.js
@@ -0,0 +1,13 @@
+import Setting from './Setting'
+
+afterAll(async () => {
+  await cleanup()
+})
+
+describe('Fragments: Setting.js', () => {
+  test('It should render Setting screen', () => {
+    const { container } = render(<Setting />)
+
+    expect(container).toBeInTheDocument()
+  })
+})
