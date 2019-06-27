14a8377c95ce11933bb9c3c95777dc52ecb9980b
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-06-27 23:38:51 +0700

    feat: add Setting to list fragments

--- app/src/fragments/index.js
+++ app/src/fragments/index.js
@@ -2,5 +2,6 @@ import Detail from './Detail'
 import Edit from './Edit'
 import Shell from './Shell'
 import Onboarding from './Onboarding'
+import Setting from './Setting'
 
-export { Detail, Edit, Shell, Onboarding }
+export { Detail, Edit, Shell, Onboarding, Setting }
