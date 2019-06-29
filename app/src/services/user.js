c0d228f24b33dc05d0a0ed18c49511734668f022
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-06-29 07:37:46 +0700

    style: add comment for each function and fixing some consistency

--- app/src/services/user.js
+++ app/src/services/user.js
@@ -2,6 +2,11 @@ import { userDb } from '../db'
 import { USER_ID_FROM_LS } from '../constants'
 import { generateUuid } from '../utils.js'
 
+/**
+ * Get existing user data
+ * @param {string} userId - an unique text that refer to our data
+ * @return {Object} PromiseObject
+ */
 export async function getUser(userId) {
   try {
     const user = await userDb.get(userId)
@@ -11,14 +16,32 @@ export async function getUser(userId) {
   }
 }
 
+/**
+ * Update existing user data
+ * but instead mutating existing, it will create the new ones
+ *
+ * visualize:
+ *
+ * 1. get the data we want to mutate/update: [data]
+ * 2. get the "revision id" for that: [data._rev]
+ * 3. update old data with the existing ones: [newData]
+ * 4. assign the _rev to the newest "revision id"
+ *
+ * @params {Object} payload - required data for creating new subscription
+ * @params {string} payload._id - an unique uuid-style text that refer to the data we want change
+ * @params {string} payload._rev - a uuid-style text taken from the data we want change
+ * @params {Date} payload.createdAt - an unix-style timestamp when this subscription being created
+ * @params {string} payload.name - name of user title. e.g: Spotify
+ */
 export async function updateUser(payload) {
-  const { _id, _rev, created_at, name } = payload
+  const { _id, _rev, createdAt, name } = payload
   try {
     const updateUser = await userDb.put({
       _id,
       _rev,
-      created_at,
-      name
+      createdAt,
+      name,
+      updatedAt: Date.now()
     })
     return updateUser
   } catch (err) {
@@ -26,29 +49,47 @@ export async function updateUser(payload) {
   }
 }
 
-// We use this for the next version
-// user can with multiple account at once
+/**
+ * get userId from localStorage
+ * We use this for the next version
+ * so user can login with multiple account at one app
+ *
+ * @return {string} uuid-style text represent current loggedin user
+ */
 export function getUserIdFromLS() {
   const userId = window.localStorage.getItem(USER_ID_FROM_LS)
   return userId
 }
 
+/**
+ * Check is user was logged in to our app?
+ * this useful for checking their first-time usage
+ *
+ * @return {Object} PromiseObject
+ */
 export async function checkLogin() {
+  const userId = getUserIdFromLS()
   try {
-    const user = await userDb.allDocs()
+    const user = await userDb.get(userId)
     return user
   } catch (err) {
     throw err
   }
 }
 
+/**
+ * Register user
+ * this is frequently being called on Onboarding fragment
+ *
+ * @return {Object} PromiseObject
+ */
 export async function registerUser() {
   try {
     const newUser = await userDb.put({
       _id: generateUuid(),
       name: null,
-      created_at: Date.now(),
-      updated_at: null
+      createdAt: Date.now(),
+      updatedAt: null
     })
     localStorage.setItem(USER_ID_FROM_LS, newUser.id)
     return newUser
