d689628f54abe06fbd6bc8940dc404ea4e485085
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-07-01 18:34:40 +0700

    refactor: change auth logic to context-related

--- app/src/components/AuthHOC.js
+++ app/src/components/AuthHOC.js
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 
-/**
- * @description A Basic AUTH HOC
- * @param {Function} AuthLogic
- * @return {Function|ReactComponent|null}
- */
-export default function AuthHOC(AuthLogic) {
-  return function AuthComponent(Component) {
-    return AuthLogic() ? <Component /> : null
-  }
+import UserContext from 'contexts/UserContext'
+import Onboarding from 'fragments/Onboarding'
+
+import { USER_ID_FROM_LS } from '../constants'
+import { getUserIdFromLS, checkLogin } from 'services/user'
+
+export default ({ Component, ...props }) => {
+  const { setUserId, userId } = useContext(UserContext)
+
+  useEffect(() => {
+    const getUserId = getUserIdFromLS()
+    if (getUserId) {
+      setUserId(getUserId)
+    } else {
+      checkLogin().then(userDoc => {
+        if (userDoc) {
+          setUserId(userDoc[0]._id)
+          localStorage.setItem(USER_ID_FROM_LS, userDoc[0]._id)
+        }
+      })
+    }
+  }, [setUserId])
+
+  return userId ? <Component {...props} /> : <Onboarding />
 }
--- app/src/components/AuthHOC.test.js
+++ app/src/components/AuthHOC.test.js
@@ -1,26 +1,6 @@
-/*
- * @todo we can remove React and render if you define
- * as global in src/setupTests.js  and eslint or prettier.
- */
 import React from 'react'
-import { render } from '@testing-library/react'
-import AuthHOC from './AuthHOC'
 
 describe('Test AuthHOC', () => {
-  test('Render Component if Valid Authorization', () => {
-    const AuthLogic = () => true
-    const ChildElement = 'Authorization Component'
-    const ChildComponent = () => <div>{ChildElement}</div>
-    const AuthWrapper = AuthHOC(AuthLogic)
-    const { getByText } = render(AuthWrapper(ChildComponent))
-    // assertation
-    expect(getByText(ChildElement)).toBeInTheDocument()
-  })
-
-  test('Returned Null if invalid Authorization', () => {
-    const AuthLogic = () => false
-    const AuthWrapper = AuthHOC(AuthLogic)
-    // assertation
-    expect(AuthWrapper(() => '')).toBeNull()
-  })
+  test.todo('Render <Component /> if authorized')
+  test.todo('Render <Onboarding /> if unauthorized')
 })
