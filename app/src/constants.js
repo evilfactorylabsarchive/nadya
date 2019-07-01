39ee7dfd9340a737d9ceeacfe07edf06c1bc4425
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-07-01 18:32:33 +0700

    feat: add constants for collection type identifier

--- app/src/constants.js
+++ app/src/constants.js
@@ -1,3 +1,5 @@
 export const IS_TEST = process.env.NODE_ENV === 'test'
 export const APP_VER = process.env.REACT_APP_NADYA_VER || 'dev'
 export const USER_ID_FROM_LS = '@nadya:userId'
+export const SUBSCRIPTIONS = 'subscriptions'
+export const USER = 'user'
