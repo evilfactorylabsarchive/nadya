b513669dbbb46b0c7990772e05027b0387185e6d
Fariz Rizaldy <farizrizaldy@outlook.co.id>
2019-07-05 13:59:03 +0700

    tests: update test without updating current snapshot

--- app/src/fragments/Edit.test.js
+++ app/src/fragments/Edit.test.js
@@ -1,12 +1,25 @@
 import Edit from './Edit'
+import { StateMock } from '@react-mock/state'
 
 afterAll(async () => {
   await cleanup()
 })
 
+const renderEdit = ({ subscription }) =>
+  render(
+    <StateMock state={{ subscription }}>
+      <Edit />
+    </StateMock>
+  )
+
 describe('Fragments: Edit.js', () => {
   test('It should render Edit screen', () => {
-    const { container } = render(<Edit />)
+    const { container } = renderEdit({
+      subscription: {
+        title: '',
+        firstBill: new Date(1562000400000)
+      }
+    })
 
     expect(container).toBeInTheDocument()
     expect(container).toMatchSnapshot()
